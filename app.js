3dec154dde6107dddf88d5bb0e19b7f670a9b547
Dannerdk21 <frederikandersen21@gmail.com>
2017-02-03 22:11:04 +0100

    added working subgroups

--- app.js
+++ app.js
@@ -15,6 +15,30 @@ app.get('/', function (req, res) {
   res.render('home');
 })
 
+app.get('/games', function (req, res) {
+  res.render('games');
+})
+
+app.get('/forums', function (req, res) {
+  res.render('forums');
+})
+
+app.get('/leaderboards', function (req, res) {
+  res.render('leaderboards');
+})
+
+app.get('/media', function (req, res) {
+  res.render('media');
+})
+
+app.get('/buy', function (req, res) {
+  res.render('buy');
+})
+
+app.get('/modding', function (req, res) {
+  res.render('modding');
+})
+
 app.get('/about', function (req, res) {
   res.render('view1');
 })
