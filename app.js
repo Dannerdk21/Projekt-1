c2d1f3fd4e2dd46e9241da44c80dd0e238ce5ee7
Dannerdk21 <frederikandersen21@gmail.com>
2017-02-03 21:41:47 +0100

    move files

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+// Load static assets file
+app.use(express.static('assets'));
+
+// View engine
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+// Routing
+app.get('/', function (req, res) {
+  res.render('home');
+})
+
+app.get('/about', function (req, res) {
+  res.render('view1');
+})
+
+// Start service
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
